c6fde165536b296798ff71be2f63656c1380da3d
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-20 22:13:28 +0300

    feat: Create timer as a template

--- src/components/editor/NavigationBar/Timer.tsx
+++ src/components/editor/NavigationBar/Timer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Timer() {
   return (
-    <span className="py-2 px-5 flex-1 bg-zinc-800 rounded-md text-zinc-400 text-center text-lg font-normal">
+    <span className="py-2 px-5 flex-1 bg-zinc-800 rounded-md text-zinc-400 text-center text-sm font-normal">
       00:00:00
     </span>
   );
