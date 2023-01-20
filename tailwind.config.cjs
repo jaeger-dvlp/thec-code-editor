9b41498f8d5c2800096585df2f18f6a0d3e04c89
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-20 17:13:42 +0300

    def: plugin twcss-typo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pop: ["Poppins", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
