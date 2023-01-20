7f182804bf67005486925b1f0d93e6840d8fee57
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-20 17:12:28 +0300

    feat: Create timer as a template comp

---
+++ src/components/editor/NavigationBar/Timer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Timer() {
+  return (
+    <span className="py-2 px-5 flex-1 bg-zinc-800 rounded-md text-zinc-400 text-center text-lg font-normal">
+      00:00:00
+    </span>
+  );
+}
+
+export default Timer;
