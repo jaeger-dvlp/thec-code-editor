2f8d59bddd44032e5cbd2754d05a6bda81f5b62f
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-20 22:11:58 +0300

    def: new rule

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,5 +8,11 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+  ],
 };
