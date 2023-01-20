f7bce31733229a6f4100b978396d33e65a6ab5fb
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-20 17:12:39 +0300

    feat: create Stack as a template comp

---
+++ src/components/editor/NavigationBar/Stack.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Stack() {
+  return (
+    <span className="py-2 px-5 flex-1 bg-zinc-800 rounded-md text-zinc-400 text-center text-lg font-normal">
+      javascript
+    </span>
+  );
+}
