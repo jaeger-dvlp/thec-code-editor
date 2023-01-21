2920682d01662cd59f1815a86660459da576679c
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-21 13:57:30 +0300

    lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
 import LanguageWrapper from "./components/context/LanguageContext";
+
+import App from "./App";
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
