5c61c0100873f73ea4f2d86e94b419325cc1fc6c
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-22 21:47:46 +0300

    feat: mock auth

--- src/components/views/Main.tsx
+++ src/components/views/Main.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { usePopup } from "@/contexts/PopupContext";
+import { useNavigate } from "react-router-dom";
 
 function Main() {
-  return <div>Main</div>;
+  const { ActivateAlertPopup } = usePopup();
+  const Navigate = useNavigate();
+
+  React.useEffect(() => {
+    ActivateAlertPopup({
+      content: "Authenticating...",
+      isLoading: true,
+    });
+
+    setTimeout(() => {
+      ActivateAlertPopup({
+        content: "Authenticated!",
+      });
+
+      Navigate("/editor");
+    }, 1500);
+  }, []);
+
+  return (
+    <div className="w-full font-pop min-h-screen relative dark:bg-[#101926] bg-white p-0 m-0 flex flex-wrap" />
+  );
 }
 
 export default Main;
