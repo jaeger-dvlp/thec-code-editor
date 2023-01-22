26abe82a6b7e96ade134bded152da43918b6cf4c
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-22 21:49:01 +0300

    rm usuned

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function App() {
-  return (
-    <div className="w-full font-pop min-h-screen relative dark:bg-[#101926] bg-white p-0 m-0 flex flex-wrap" />
-  );
-}
-
-export default App;
