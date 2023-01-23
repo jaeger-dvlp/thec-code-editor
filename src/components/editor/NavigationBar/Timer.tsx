033982086acb55f033af733c622867901588a3f6
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-23 09:49:18 +0300

    feat: Create Timer Hook

--- src/components/editor/NavigationBar/Timer.tsx
+++ src/components/editor/NavigationBar/Timer.tsx
@@ -1,9 +1,38 @@
 import React from "react";
 
+export function UseTimer() {
+  const [time, setTime] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const timestamp = new Date(time * 1000).toISOString().substr(11, 8);
+
+  const seconds = time % 60;
+  const minutes = Math.floor(time / 60) % 60;
+  const hours = Math.floor(time / 3600);
+
+  return {
+    seconds,
+    minutes,
+    hours,
+    timestamp,
+  };
+}
+
 function Timer() {
+  const { seconds, minutes, hours } = UseTimer();
+
   return (
     <span className="py-2 px-5 flex-1 dark:bg-[#070f1c] bg-[#F3F4F6] rounded-md text-zinc-400 text-center text-sm font-normal">
-      00:00:00
+      {`${hours.toString().padStart(2, "0")}:${minutes
+        .toString()
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}
     </span>
   );
 }
