25ffce57c95d59b2400349971666ae479eb31018
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-23 15:52:24 +0300

    feat: handle session

--- src/components/views/Main.tsx
+++ src/components/views/Main.tsx
@@ -1,51 +1,100 @@
 import React from "react";
 import { useIntl } from "react-intl";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import useAuth from "@/common/hooks/useAuth";
 import { usePopup } from "@contexts/PopupContext";
 import useBrowser from "@/common/hooks/useBrowser";
+import useLocalStore from "@/common/hooks/useLocalStore";
+
+function useQuery() {
+  const { search } = useLocation();
+
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
 
 function Main() {
   const Navigate = useNavigate();
   const { authUser } = useAuth();
   const { isMobile } = useBrowser();
+  const { getItem } = useLocalStore();
   const { formatMessage: t } = useIntl();
+  const SessionID = useQuery().get("session_id");
   const { ActivateAlertPopup, DeactivateAlertPopup } = usePopup();
 
+  function MobileHandler() {
+    if (isMobile === null) return null;
+
+    if (isMobile) {
+      ActivateAlertPopup({
+        content: t({ id: "errors.mobile-device" }),
+        onClick: () => {
+          window.location.href = "/";
+        },
+      });
+
+      return null;
+    }
+
+    return true;
+  }
+
+  const userHandler = async () => {
+    if (!SessionID && !getItem("user")) {
+      return ActivateAlertPopup({
+        content: t({ id: "errors.invalid-session-id" }),
+        onClick: () => {
+          Navigate("/");
+        },
+      });
+    }
+
+    const isSessionValid = await authUser(SessionID);
+
+    if (!isSessionValid) {
+      return ActivateAlertPopup({
+        content: t({ id: "errors.invalid-session-id" }),
+        onClick: () => {
+          Navigate("/");
+        },
+      });
+    }
+
+    return true;
+  };
+
+  const redirectUser = () => {
+    setTimeout(() => {
+      ActivateAlertPopup({
+        content: t({ id: "popups.authenticated" }),
+        isLoading: true,
+      });
+    }, 1000);
+
+    return setTimeout(() => {
+      DeactivateAlertPopup();
+      return Navigate("/editor");
+    }, 2500);
+  };
+
   React.useEffect(() => {
-    const handleRun = () => {
+    const handleRun = async () => {
       ActivateAlertPopup({
         content: t({ id: "popups.authenticating" }),
         isLoading: true,
       });
 
-      if (isMobile === null) return null;
+      const isUserValid = await userHandler();
+      const isMobileValid = MobileHandler();
 
-      if (isMobile) {
-        return ActivateAlertPopup({
-          content: t({ id: "errors.mobile-device" }),
-          onClick: () => {
-            window.location.href = "/";
-          },
-        });
-      }
+      if (!isUserValid) return null;
+      if (!isMobileValid) return null;
 
-      authUser({
-        user_id: "1",
-        full_name: "Test TEST",
-        username: "testTest",
-        authToken: Math.floor(Math.random() * 99999).toString(),
-      });
-
-      return setTimeout(() => {
-        DeactivateAlertPopup();
-        return Navigate("/editor");
-      }, 1500);
+      return redirectUser();
     };
 
     handleRun();
-  }, [isMobile]);
+  }, [isMobile, SessionID]);
 
   return (
     <div className="w-full font-pop min-h-screen relative dark:bg-[#101926] bg-white p-0 m-0 flex flex-wrap" />
