9f6c99740ac2cd296c8dfe77be3a7d252f7c5789
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-23 15:50:38 +0300

    feat: auth with service

--- src/common/hooks/useAuth.tsx
+++ src/common/hooks/useAuth.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { User } from "@/common/types/types";
 import useUser from "@/common/hooks/useUser";
 import useLocalStore from "@/common/hooks/useLocalStore";
+import apiService from "../services/api.service";
 
 export default function useAuth() {
   const { user, addUser, removeUser } = useUser();
@@ -15,8 +15,34 @@ export default function useAuth() {
     }
   }, []);
 
-  const authUser = (userToAuth: User) => {
-    addUser(userToAuth);
+  const authUser = async (id: string | null) => {
+    const localUser = getItem("user");
+
+    if (localUser) {
+      const { sessionId } = JSON.parse(localUser);
+      const { isValid } = await apiService.authSession(sessionId);
+
+      if (isValid) {
+        return true;
+      }
+
+      removeUser();
+    }
+
+    const { isValid, sessionId, userId, challengeId } =
+      await apiService.authSession(id);
+
+    if (!isValid) {
+      return false;
+    }
+
+    addUser({
+      userId,
+      sessionId,
+      challengeId,
+    });
+
+    return true;
   };
 
   const unAuthUser = () => {
