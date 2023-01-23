3ebac18289131f6f967e0d554efaa2ece4564308
jaeger-dvlp <omer.kayalar0101@icloud.com>
2023-01-23 15:45:01 +0300

    feat: authorization

--- src/common/configs/api.config.ts
+++ src/common/configs/api.config.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-const { API_URL: apiUrl } = import.meta.env;
+const { VITE_API_URL: apiUrl, VITE_API_TOKEN: apiToken } = import.meta.env;
 
 const API = axios.create({
   baseURL: apiUrl,
+  headers: {
+    authorization: `Bearer ${apiToken}`,
+  },
 });
 
 export default API;
