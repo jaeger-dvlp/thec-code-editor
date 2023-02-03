ff864267771897afbd77abbd6f8d2869197ed332
JAEGER_ <omer.kayalar0101@icloud.com>
2023-02-03 09:28:12 +0300

    feat: create user handler

---
+++ src/common/handlers/user.handler.tsx
@@ -0,0 +1,59 @@
+import { useIntl } from "react-intl";
+import useAuth from "@/common/hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
+import useQuery from "@/common/hooks/useQuery";
+import { usePopup } from "@/contexts/PopupContext";
+import useLocalStore from "@/common/hooks/useLocalStore";
+
+function userHandler() {
+  const Navigate = useNavigate();
+  const { authUser } = useAuth();
+  const { getItem } = useLocalStore();
+  const { formatMessage: t } = useIntl();
+  const { ActivateAlertPopup } = usePopup();
+  const SessionID = useQuery().get("session_id");
+
+  const handle = async () => {
+    if (!SessionID && !getItem("user")) {
+      ActivateAlertPopup({
+        content: t({ id: "errors.invalid-session-id" }),
+        onClick: () => {
+          Navigate("/");
+        },
+      });
+
+      return {
+        isValid: false,
+        isStarted: false,
+      };
+    }
+
+    const { isValid, isStarted } = await authUser(SessionID);
+
+    if (!isValid) {
+      ActivateAlertPopup({
+        content: t({ id: "errors.invalid-session-id" }),
+        onClick: () => {
+          Navigate("/");
+        },
+      });
+
+      return {
+        isValid: false,
+        isStarted: false,
+      };
+    }
+
+    return {
+      isValid,
+      isStarted,
+    };
+  };
+
+  return {
+    handle,
+  };
+}
+
+export default userHandler;
