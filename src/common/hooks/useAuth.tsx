26c95ca0f2a15459cd4e1e49dae2de7049bf1ff5
JAEGER_ <omer.kayalar0101@icloud.com>
2023-02-03 12:16:49 +0300

    feat: handle local & query auth

--- src/common/hooks/useAuth.tsx
+++ src/common/hooks/useAuth.tsx
@@ -16,33 +16,60 @@ export default function useAuth() {
   }, []);
 
   const authUser = async (id: string | null) => {
-    const localUser = getItem("user");
-
-    if (localUser) {
-      const { sessionId } = JSON.parse(localUser);
-      const { isValid } = await apiService.authSession(sessionId);
+    const authWithLocalUser = async (sessionId) => {
+      const { isValid, isStarted } = await apiService.authSession(sessionId);
 
       if (isValid) {
-        return true;
+        return {
+          isValid,
+          isStarted,
+        };
       }
 
       removeUser();
+
+      return {
+        isValid: false,
+        isStarted: false,
+      };
+    };
+    const authWithQueryID = async () => {
+      const { isValid, sessionId, isStarted, error } =
+        await apiService.authSession(id);
+
+      if (error || !isValid) {
+        return {
+          isValid: false,
+          isStarted: false,
+          error,
+        };
+      }
+
+      addUser({
+        sessionId,
+      });
+
+      return {
+        isValid,
+        isStarted,
+      };
+    };
+
+    const localUser = getItem("user");
+
+    if (localUser && localUser.length > 0) {
+      const { sessionId } = JSON.parse(localUser);
+
+      if (!id) {
+        return authWithLocalUser(sessionId);
+      }
+
+      if (sessionId === id) {
+        return authWithLocalUser(sessionId);
+      }
     }
 
-    const { isValid, sessionId, userId, challengeId } =
-      await apiService.authSession(id);
-
-    if (!isValid) {
-      return false;
-    }
-
-    addUser({
-      userId,
-      sessionId,
-      challengeId,
-    });
-
-    return true;
+    return authWithQueryID();
   };
 
   const unAuthUser = () => {
