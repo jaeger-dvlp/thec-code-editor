b8b845cc2fb4f04a43f7df41a2c8b99b1d0f2432
JAEGER_ <omer.kayalar0101@icloud.com>
2023-02-03 09:27:00 +0300

    feat: create challenge session hook

---
+++ src/common/hooks/useSession.tsx
@@ -0,0 +1,98 @@
+import { useIntl } from "react-intl";
+import apiService from "../services/api.service";
+import handleError from "../utils/handleError";
+
+export default function useSession() {
+  const { formatMessage: t } = useIntl();
+
+  const getRemoteChallenge = async () => {
+    try {
+      const { sessionId } = JSON.parse(localStorage.getItem("user") as string);
+
+      if (!sessionId) {
+        throw new Error("Session id not found!");
+      }
+
+      const {
+        isValid,
+        challenge: { challenges: challenge },
+      } = await apiService.getChallenge(sessionId);
+
+      if (!isValid) {
+        throw new Error("Session id not valid!");
+      }
+
+      return { challenge };
+    } catch (error) {
+      return { error: handleError(error, t) };
+    }
+  };
+
+  const getLocalChallenge = async () => {
+    try {
+      const localItem = JSON.parse(localStorage.getItem("user") as string);
+
+      if (!localItem) {
+        throw new Error("User not found!");
+      }
+
+      const { challenge } = localItem;
+
+      if (!challenge) {
+        throw new Error("Challenge not found!");
+      }
+
+      return { challenge };
+    } catch (error) {
+      return { error: handleError(error, t) };
+    }
+  };
+
+  const setChallenge = async (challenge: any) => {
+    try {
+      const localItem = JSON.parse(localStorage.getItem("user") as string);
+
+      if (!localItem) {
+        throw new Error("User not found!");
+      }
+
+      const newItem = {
+        ...localItem,
+        challenge,
+      };
+
+      localItem.setItem("user", JSON.stringify(newItem));
+
+      return { challenge };
+    } catch (error) {
+      return { error: handleError(error, t) };
+    }
+  };
+
+  const getAndSetChallenge = async () => {
+    try {
+      const { challenge, error } = await getRemoteChallenge();
+
+      if (error) {
+        throw new Error(error);
+      }
+
+      if (!challenge) {
+        throw new Error("Challenge not found!");
+      }
+
+      setChallenge(challenge);
+
+      return { challenge };
+    } catch (error) {
+      return { error: handleError(error, t) };
+    }
+  };
+
+  return {
+    getRemoteChallenge,
+    getLocalChallenge,
+    setChallenge,
+    getAndSetChallenge,
+  };
+}
